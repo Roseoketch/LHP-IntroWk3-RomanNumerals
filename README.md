ef0a30695fac81d917d8926b076b7176dde59fcd
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-20 00:16:20 -0700

    translate numbers except for 4/9 correctly

--- README.md
+++ README.md
@@ -1,32 +1,26 @@
 # [Roman Numerals](http://karenfreemansmith.github.io/roman-numbers)
 ![project screenshot](/img/screenshot.jpg)
 
-__Version 1: April 18, 2016__
+__Version 1: April 19, 2016__
 ## by [Karen Freeman-Smith](http://karenfreemansmith.github.io)
 
 ### Description
 __*A website that translates numbers into Roman-numerals.*__
 
-Write a method to convert numbers into Roman numerals.
-
-For some background, Roman numerals are based on seven symbols:
-
-Symbol  Value
-I       1
-V       5
-X       10
-L       50
-C       100
-D       500
-M       1,000
-
-The most basic rule is that you add the value of all the symbols: so II is 2, LXVI is 66, etc.
-
-The exception is that you can't have more than three of the same character is a row: instead, you switch to subtraction. So instead of writing IIII for 4, you write IV (for 5 minus 1); and instead of writing LXXXX for 90, you write XC.
-
-You also have to separate ones, tens, hundreds, and thousands. In other words, 99 is XCIX, not IC. With Roman numerals, you can't count higher than 3,999 in Roman numerals.
-
 Draft some input-output examples to brainstorm the behaviors that you'll want to capture in your application. Tackle them one-by-one, worrying only about the future behaviors once the current example is implemented.
+
+#### Tests:
+* 1000 | "M", 2000 | "MM", 3000 | "MMM"
+* 2500 | "MMD", 500 | "D"
+* 3800 | "MMDCCC", 1300 | "MCCC"
+* 1250 | "MCCL", 850 | "DCCCL", 50 | "L"
+* 2510 | "MMDX", 360 | "CCCLX", 20 | "XX"
+* 3505 | "MMMDV", 735 | "DCCXXXV", 8 | "VIII"
+* 1001 | "MI", 852 | "DCCCLII",
+* 
+* non-numeric input is rejected
+* 4000 gives an error
+
 
 #### Uses:
 * [Fresh Start](http://karenfreemansmith.github.io/freshstart)
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,133 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+function romanize(yourNumber) {
+  yourNumber = parseInt(yourNumber);
+  var romanNumber = "";
+  var placeholder = 0;
+
+  if(yourNumber >3999) {
+    romanNumber = "Romans didn't count over 3,999!";
+  } else {  // 1000's place 1-3, 4+ not allowed
+    if(yourNumber > 999) {
+      placeholder=parseInt(yourNumber/1000);
+      yourNumber = yourNumber % 1000;
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="M";
+      }
+    } // 100's place: 9, 5-8, 4, 1-3
+    if(yourNumber > 99) {
+      placeholder=parseInt(yourNumber/100);
+      yourNumber = yourNumber % 100;
+      if(placeholder>=5) {
+        romanNumber+="D";
+        placeholder-=5;
+      }
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="C";
+      }
+    } // 10's place: 9, 5-8, 4, 1-3
+    if(yourNumber > 9) {
+      placeholder=parseInt(yourNumber/10);
+      yourNumber = yourNumber % 10;
+      if(placeholder>=5) {
+        romanNumber+="L";
+        placeholder-=5;
+      }
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="X";
+      }
+    } // 1's place: 9, 5-8, 4, 1-3
+    placeholder=parseInt(yourNumber);
+    if(placeholder>=5) {
+      romanNumber+="V";
+      placeholder-=5;
+    }
+    for(i=1;i<=placeholder;i++) {
+      romanNumber+="I";
+    }
+  }
+  return romanNumber;
+}
+
+$("#convertNumber").click(function() {
+
+/*
+
+  var romanNumber = "";
+  var placeholder = 0;
+
+  if(yourNumber>=1000) {
+
+  }
+  if(yourNumber>=500) {
+    placeholder=parseInt(yourNumber/500);
+    yourNumber = yourNumber % 500;
+        alert(placeholder + ": " + yourNumber);
+    if((placeholder===4) || (yourNumber >= 400)) {
+      romanNumber+="CM";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="D";
+      }
+    }
+  }
+  if(yourNumber>=100) {
+    placeholder=yourNumber/100;
+    yourNumber = yourNumber % 100;
+    if(placeholder===4) {
+      romanNumber+="XC";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="C";
+      }
+    }
+  }
+  if(yourNumber>=50) {
+    placeholder=yourNumber/50;
+    yourNumber = yourNumber % 50;
+    if(placeholder===4) {
+      romanNumber+="XC";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="L";
+      }
+    }
+  }
+  if(yourNumber>=10) {
+    placeholder=yourNumber/10;
+    yourNumber = yourNumber % 10;
+    if(placeholder===4) {
+      romanNumber+="IL";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="X";
+      }
+    }
+  }
+  if(yourNumber>=5) {
+        alert(yourNumber);
+    placeholder=yourNumber/5;
+    yourNumber = yourNumber % 5;
+
+    if(placeholder===4) {
+      romanNumber+="IX";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="V";
+      }
+    }
+  }
+  if(yourNumber>=1) {
+    placeholder=yourNumber/1;
+    yourNumber = yourNumber % 1;
+    if(placeholder===4) {
+      romanNumber+="IV";
+    } else {
+      for(i=1;i<=placeholder;i++) {
+        romanNumber+="I";
+      }
+    }
+  }
+  */
+
+  $("#results").text(romanize($("#myNumber").val()));
+});
